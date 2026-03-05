512f1530fd6400b995c9816ef04139184d81b1c7
Matthew <149590049+MatthewW2027@users.noreply.github.com>
2026-03-05 19:24:51 +0000

    4.11 Notes

--- Main.java
+++ Main.java
@@ -1,6 +1,39 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+
+      // *** 2D ARRAY DATA STRUCTURE *** 
+      // like an Array, but a GRID 
+      // ROWS are (horizontal) & COLUMNS (vertical)
+
+      // DECLARE a 2D array with double square brackets 
+      int[][] experimentData; // null (empty)
+      double[][] pointValues; 
+
+      // CREATE/INITIALIZE a 2D array, call constructor 
+      experimentData = new int [10][3]; // 10 rows, 3 cols 
+
+      // INTIALIZE a 2D array (declare + create) 
+      String[][] seatingChart = new String[4][6];
+      // total number of items? 4 x 6 = 24 
+
+      // SET A VALUE by accessing a cell 
+      // ACCESS a cell: arrayName[row][col] 
+      seatingChart[0][0] = "Mia"; 
+      seatingChart[0][5] = "Sohaila"; 
+      // 6 cols -> but 5 is FINAL INDEX!!!
+      seatingChart[1][0] = "Ethan";
+      seatingChart[1][5] = "Zach";
+      seatingChart[2][1] = "Aaniyah";
+      seatingChart[2][3] = "Simeon"; 
+      seatingChart[2][4] = "Bowe"; // second to last col 
+      seatingChart[3][0] = "Schwartz"; 
+      seatingChart[3][1] = "Caleb"; 
+      seatingChart[3][2] = "Dolan"; 
+      seatingChart[3][3] = "Kyanni"; 
+      seatingChart[3][4] = "Danny"; 
+      seatingChart[3][5] = "Matthew"; 
+      System.out.println(seatingChart[3][5]);  
+
    }
 }
